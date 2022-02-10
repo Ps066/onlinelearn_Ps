f94e70597fbd5ef15e81e59cfba0e89149687596
Prashant shukla <81038824+Ps066@users.noreply.github.com>
2022-02-10 11:24:30 +0530

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -8,6 +8,13 @@ from .models import Course, Lesson, Instructor, Learner,Choice ,Question
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
+    
+class QuestionInline(admin.StackedInline):
+    model = Question
+    
+    
+class ChoiceInline(admin.StackedInline):
+    model = Choice
 
 
 # Register your models here.
@@ -20,6 +27,9 @@ class CourseAdmin(admin.ModelAdmin):
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
+    
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
 
 
 # <HINT> Register Question and Choice models here
@@ -28,5 +38,6 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
-admin.site.register(Question)
+
